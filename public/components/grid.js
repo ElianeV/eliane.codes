50549816db57174030144672e5fd6be2f52eff02
Eliane V <eliane.vdc@protonmail.com>
2024-01-20 18:01:16 +0000

    Functioning border animation

--- public/components/grid.js
+++ public/components/grid.js
@@ -3,156 +3,332 @@ import { Oswald } from "next/font/google";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
-function Grid(gridClassName) {
+function Grid({ infront }) {
+  console.log(infront);
   return (
-    <div className={gridClassName}>
+    <div
+      className={`${styles.gridcontainer} ${
+        infront ? styles.positionfront : styles.positionback
+      }`}
+    >
       {/* ROW 1*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={`${styles.appear} ${styles.delay1}`}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={`${styles.appear} ${styles.delay2}`}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 2*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div className={infront ? styles.bordertransparent : styles.borderdelay4}>
         ELIANE.CODES \<br />
         HOME
       </div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent} style={{ textAlign: "right" }}>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+        style={{ textAlign: "right" }}
+      >
         Github <br />
         Linkedin
       </div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 3*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 4*/}
 
-      <div className={styles.bordertransparent}></div>
       <div
-        className={`${styles.bordertransparent} ${styles.navigationelement}`}
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        } ${styles.navigationelement}`}
       >
         MY WORK
       </div>
-      <div className={styles.bordertransparent}></div>
       <div
-        className={`${styles.bordertransparent} ${styles.tagline} ${oswald.className}`}
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        } ${styles.tagline} ${oswald.className}`}
       >
         WEB
       </div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
       <div
-        className={`${styles.bordertransparent} ${styles.tagline} ${oswald.className}`}
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        } ${styles.tagline} ${oswald.className}`}
         style={{ paddingRight: "8px" }}
       >
         DEV<span style={{ color: "#CB450C" }}>EL</span>OP
       </div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 5*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 6*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
       <div
-        className={`${styles.bordertransparent} ${styles.tagline} ${oswald.className}`}
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        } ${styles.tagline} ${oswald.className}`}
         style={{ textAlign: "right", paddingRight: "4px" }}
       >
         DES<span style={{ color: "#CB450C" }}>I</span>GN
       </div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 7*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 8*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
       <div
-        className={`${styles.bordertransparent} ${styles.tagline} ${oswald.className}`}
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        } ${styles.tagline} ${oswald.className}`}
         style={{ textAlign: "right", paddingRight: "4px" }}
       >
         <span style={{ color: "#CB450C" }}>AN</span>D
       </div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 9*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 10*/}
 
-      <div className={styles.bordertransparent}></div>
       <div
-        className={`${styles.bordertransparent} ${styles.navigationelement}`}
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        } ${styles.navigationelement}`}
       >
         <a href="mailto:eliane.vdc@protonmail.com">CONTACT</a>
       </div>
-      <div className={styles.bordertransparent}></div>
-      <div className={`${styles.biography} ${styles.bordertransparent}`}>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${styles.biography} ${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        }`}
+      >
         <div style={{ maxWidth: "370px" }}>
           Building websites and applications with a focus on impactful
           communication through design. Employing a collaborative and thoughtful
@@ -161,27 +337,51 @@ function Grid(gridClassName) {
       </div>
       <div className={styles.biography}></div>
       <div className={styles.biography}></div>
-      <div className={styles.bordertransparent}></div>
       <div
-        className={`${styles.bordertransparent} ${styles.tagline} ${oswald.className}`}
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={`${
+          infront ? styles.bordertransparent : styles.borderdelay4
+        } ${styles.tagline} ${oswald.className}`}
         style={{ textAlign: "right", paddingRight: "8px" }}
       >
         ELEVAT<span style={{ color: "#CB450C" }}>E</span>
       </div>
 
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
 
       {/* ROW 11*/}
 
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
-      <div className={styles.bordertransparent}></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
+      <div
+        className={infront ? styles.bordertransparent : styles.borderdelay4}
+      ></div>
     </div>
   );
 }
