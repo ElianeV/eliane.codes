5fb4b69cc7dbe91952bb5fedce01ccfd40b0b637
Eliane V <eliane.vdc@protonmail.com>
2024-02-08 19:46:41 +0000

    Create my work grid system

--- app/my-work/page.js
+++ app/my-work/page.js
@@ -1,5 +1,109 @@
+import styles from "../../app/page.module.css";
+import { Oswald } from "next/font/google";
+import Link from "next/link";
+
+const oswald = Oswald({ subsets: ["latin"] });
+
 const Work = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className={styles.myworkcontainer}>
+      {/* ROW 1*/}
+
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 2*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 3*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 4*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 5*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 6*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 7*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 8*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 9*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 10*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 11*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 12*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 13*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 14*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 15*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 16*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 17*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 18*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+
+      {/* ROW 19*/}
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+      <div className={styles.borderdelay8}></div>
+    </div>
+  );
 };
 
 export default Work;
