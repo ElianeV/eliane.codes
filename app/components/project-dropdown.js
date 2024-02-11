4139758da5d398c97d3b00a78c22b46168b0ab74
Eliane V <eliane.vdc@protonmail.com>
2024-02-11 17:41:21 +0000

    Project container design finished

--- app/components/project-dropdown.js
+++ app/components/project-dropdown.js
@@ -10,22 +10,34 @@ function ProjectDropdown({ name, type, role, linktext, url, DD, setDD }) {
         onClick={() => {
           setDD(!DD);
         }}
-        className={`${styles.borderdelay8}`}
+        className={`${styles.borderdelay8} ${styles.projectcontainer}`}
       >
         <div className={`${styles.titlecontainer}`}>
-          <div className={`${styles.projecttitle} ${oswald.className}`}>
+          <h2 className={`${styles.projecttitle} ${oswald.className}`}>
             {name}
-          </div>
-          <div className={styles.projectinfo}>
+          </h2>
+          <p className={styles.projectinfo}>
             {type}
             <br />
             {role}
-          </div>
+          </p>
           <a className={styles.projectlink} href={url}>
             {linktext}
           </a>
         </div>
-        <div className={`${DD ? styles.block : styles.hidden}`}>Hello</div>
+        <div className={`${DD ? styles.block : styles.hidden}`}>
+          <div className={styles.projectimage}></div>
+          <div style={{ marginLeft: "32px" }}>
+            <h3>ABOUT</h3>
+            <p style={{ paddingTop: "16px" }}>Some text here</p>
+          </div>
+          <div style={{ marginLeft: "32px" }}>
+            <h3>STACK</h3>
+            <p style={{ paddingTop: "16px" }}>
+              Design: something, something; something,...
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
